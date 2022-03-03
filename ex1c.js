e6a9d7859c10316499e81c1e13d50b1e436af6ca
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-03 13:59:33 +0100

    solucionado

--- ex1c.js
+++ ex1c.js
@@ -1,8 +1,8 @@
 // Investiga cómo usar el método indexOf. 
 
-let resultado1 = "Carambola"; // modifica esta línea, usa indexOf para buscar la posición la primera letra 'a'
+let resultado1 = "Carambola".indexOf("a"); // modifica esta línea, usa indexOf para buscar la posición la primera letra 'a'
 console.log("Debería dar '1'", resultado1)
 
-let resultado2 = "Carambola"; // modifica esta línea, usa indexOf para buscar la posición de la letra 'l'
+let resultado2 = "Carambola".indexOf("l"); // modifica esta línea, usa indexOf para buscar la posición de la letra 'l'
 console.log("Debería dar '7'", resultado2)
 
